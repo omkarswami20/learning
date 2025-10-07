3c401d7b53206c46f04c2c41782b6c31856880ea
Omkar Swami <92944253+omkarswami20@users.noreply.github.com>
2025-10-07 22:45:19 +0530

    Create LearningLoops.java
    basic 100 printing ogf hellowrold

---
+++ LearningLoops.java
@@ -0,0 +1,12 @@
+package main;
+
+
+public class LearnLoops {
+  public static void main (string[]args) {
+    for(int i =0; i<100; i++)
+    {
+    system.out.printIn("Hello-World");    
+    }
+  
+  }
+}
